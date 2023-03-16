66055336c9093002d3d95b775636fb2110bd6a99
Julie M <seejulielee@gmail.com>
2023-03-16 07:41:10 -0700

    Added a catch method for errors on the image

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${data.urls.regular})`
 		    document.getElementById("credit").textContent = `Photo by: ${data.user.name}`
     })
+    .catch(err => {    
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
+    })
+
 
     
